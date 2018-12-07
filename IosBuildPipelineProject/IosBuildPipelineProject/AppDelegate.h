c67cf9911469ba69d50141723e15af6f62e55a72
muralikrish <muraliveeramosu@gmail.com>
2018-12-07 16:38:03 +0530

    Initial commit
    Added initial project.

---
+++ IosBuildPipelineProject/IosBuildPipelineProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IosBuildPipelineProject
+//
+//  Created by Murali Krishna on 07/12/18.
+//  Copyright © 2018 Murali Krishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IosBuildPipelineProject/IosBuildPipelineProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IosBuildPipelineProject
+//
+//  Created by Murali Krishna on 07/12/18.
+//  Copyright © 2018 Murali Krishna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
